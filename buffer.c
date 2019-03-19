1d98c0fe595fa486e7a148794f9799c842807d5f
TkaTheGod <47615360+TkaTheGod@users.noreply.github.com>
2019-03-19 14:32:40 -0300

    Create buffer.c

---
+++ buffer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+        char buffer1[8] = {'T','K','A','I','P','E','R','\0'};
+        char buffer2[8] = {'T','K','A','I','P','E','2','\0'};
+
+        strcpy(buffer2, argv[1]);
+        printf("\nBuffer2: %s\n", buffer2);
+        printf("Buffer1: %s\n\n", buffer1);
+
+        system(buffer1);
+
+        return 0;
+}
